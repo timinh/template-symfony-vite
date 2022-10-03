0853a6e8c31c771b6900c966a1c3f6cece276391
ROBIN Gilles <gilles.robin@uca.fr>
2022-10-03 21:38:23 +0200

    gestion favoris

--- assets/js/pages/favorites.vue
+++ assets/js/pages/favorites.vue
@@ -2,10 +2,10 @@
     <div>
         <h1>Mes favoris</h1>
         <div class="row">
-            <input type="text" v-model="searchValue" @keyup="search" placeholder="Filtrer" />
+            <input type="text" v-model="searchValue" placeholder="Filtrer" />
         </div>
         <div class="row">
-            <character-card class="m-1" v-for="character in store.bookmarks" :character="character">
+            <character-card class="m-1" v-for="character in filteredBookmarks" :character="character">
                 <button v-if="store.inBookmarks(character)" @click="store.removeBookmark(character)"
                     class="btn btn-secondary">Enlever des favoris</button>
             </character-card>
@@ -16,12 +16,15 @@
 name: favoris
 </route>
 <script setup>
-    import {useCharacterStore} from '../store/characters'
+import {useCharacterStore} from '../store/characters'
     const searchValue = ref('')
     const store = useCharacterStore()
-    const search = () => {
+    const filteredBookmarks = computed( () => {
         if(searchValue.value.length === 0 || searchValue.value.length >2 ){
-            
+            return store.bookmarks.filter( (char) => 
+                char.name.toLowerCase().includes(searchValue.value.toLowerCase())
+            )
         }
-    }
+        return store.bookmarks
+    })
 </script>
